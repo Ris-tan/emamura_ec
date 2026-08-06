71ce931b3aed3b762a68d07bf606ac5af6d82cf3
tanaka <i-tanaka@k-shared.co.jp>
2026-08-06 16:54:48 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/emamura_ec/EmamuraEcApplication.java
@@ -0,0 +1,13 @@
+package emamura_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmamuraEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmamuraEcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emamura_ec/EmamuraEcApplicationTests.java
@@ -0,0 +1,13 @@
+package emamura_ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmamuraEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
